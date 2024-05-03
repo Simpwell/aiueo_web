df06ec144b847b793ec73fbf497bc2648dbc2138
Toshiro Yamayoshi <simpwellsoft@gmail.com>
2024-05-03 23:58:20 +0900

    implement popup

--- app.js
+++ app.js
@@ -1568,9 +1568,28 @@ function adjustButtonSize() {
     renderKana();
   }
   
+
+var modal = document.querySelector("#modal");
+var modalOverlay = document.querySelector("#modal-overlay");
+var closeButton = document.querySelector("#close-button");
+var openButton = document.querySelector("#open-button");
+
+//閉じるボタン
+closeButton.addEventListener("click", function () {
+  modal.classList.toggle("closed");
+  modalOverlay.classList.toggle("closed");
+});
+
+// //開くボタン
+// openButton.addEventListener("click", function () {
+//   modal.classList.toggle("closed");
+//   modalOverlay.classList.toggle("closed");
+// });
+
   // アプリの初期化を実行
   initApp();
 
   window.onload = function() {
-    document.getElementById("pop-up").checked = true;
-  }
+    modal.classList.toggle("closed");
+    modalOverlay.classList.toggle("closed");
+    }
